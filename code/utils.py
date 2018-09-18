96b93d74bf63596d09c15cd7196029144b00a571
Zetinator <erick_zetina@hotmail.com>
2018-09-17 20:31:00 -0500

    training and test working

--- code/utils.py
+++ code/utils.py
@@ -13,9 +13,19 @@ class Settings(object):
 class data_loader(object):
     def __init__(self, dataset_path):
         self.dataset_path = dataset_path
+        self.images = None
+
     def load_images(self):
         image_paths = sorted(glob.glob(os.path.join(self.dataset_path, '*.png')))
         images = [cv2.imread(img,0) for img in image_paths]
-        images = np.array(images)
+        self.images = np.array(images)
 
-        return images
+    def get_training (self):
+        training = self.images[:int(np.ceil(self.images.shape[0]*.7)),:,:]
+
+        return training
+
+    def get_test (self):
+        test = self.images[int(np.ceil(self.images.shape[0]*.7)):,:,:]
+
+        return test
